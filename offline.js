ee74c7587f3cae312221ee3203b4e58962d2ea26
golendercaria <>
2019-04-05 19:38:12 +0200

    First commit

---
+++ offline.js
@@ -0,0 +1,47 @@
+var filesToCache = [
+	'/',
+	'/offline/',
+	'/style.css',
+	'/logo.png'
+];
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open('appsOfflineCache').then(function(cache) {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+var bad_url = ["/wp-admin/","/recaptcha/"];
+
+self.addEventListener('fetch', function(event) {
+
+	var good_url = true;
+	for(var i in bad_url){
+		if( event.request.url.indexOf(bad_url[i]) != -1 ){
+			good_url = false;
+			break;
+		}
+	}
+	
+	if( !good_url ){
+		return false;
+	}
+	
+	
+	
+  event.respondWith(
+    caches.open('appsOfflineCache').then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        })
+        return response || fetchPromise;
+      }).catch(function(){
+	      return caches.match('/offline/')
+      })
+    })
+  );
+});
